42a5eb59831025824c72c94bda0adff6a349532e
Stephen Touset <stephen@touset.org>
2011-04-19 11:39:21 -0400

    Lazy-load configuration (closes #5)

--- lib/transloadit/rails.rb
+++ lib/transloadit/rails.rb
@@ -17,14 +17,18 @@ class Transloadit::Rails < Rails::Engine
   end
   
   initializer 'transloadit.configure' do |app|
-    # preload the contents of the configuration file.
-    app.root.join(CONFIG_PATH).open do |file|
-      self.class.config = YAML.load(file)
-    end rescue nil
+    self.class.application = app
+  end
+  
+  def self.configuration
+    YAML.load_file self.application.root.join(CONFIG_PATH)
   end
   
   class << self
-    attr_accessor :config
+    attr_accessor :application
+    
+    extend ActiveSupport::Memoizable
+    memoize :configuration unless Rails.env.development?
   end
   
   #
@@ -32,8 +36,8 @@ class Transloadit::Rails < Rails::Engine
   #
   def self.transloadit
     Transloadit.new(
-      :key    => self.config['auth']['key'],
-      :secret => self.config['auth']['secret']
+      :key    => self.configuration['auth']['key'],
+      :secret => self.configuration['auth']['secret']
     )
   end
   
@@ -41,7 +45,7 @@ class Transloadit::Rails < Rails::Engine
   # Creates an assembly for the named template.
   #
   def self.template(name, options = {})
-    template = self.config['templates'].try(:fetch, name.to_s)
+    template = self.configuration['templates'].try(:fetch, name.to_s)
     
     self.transloadit.assembly case template
       when String then { :template_id => template }.merge(options)
