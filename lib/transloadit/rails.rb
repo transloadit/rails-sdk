9abaf76fd64e3e4b3d8d557618691319616f6054
Stephen Touset <stephen@touset.org>
2011-08-08 10:16:26 -0300

    Edited lib/transloadit/rails.rb via GitHub

--- lib/transloadit/rails.rb
+++ lib/transloadit/rails.rb
@@ -1,4 +1,4 @@
 require 'transloadit'
 require 'transloadit/rails/engine'
 require 'transloadit/rails/version'
-require 'transloadit/rails/view_helper'
+require 'transloadit/rails/view_helper' # TODO: fix autoloading brokenness
