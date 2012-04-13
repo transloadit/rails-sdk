9fac7440e07a6f0712ed4c1a0552f31a512c2969
Robin Mehner <robin@coding-robin.de>
2012-04-13 14:21:03 +0200

    Don't sign requests when no secret is provided.
    This fixes: http://support.transloadit.com/discussions/problems/642-unable-to-run-basic-example-from-gem-instructions

--- lib/transloadit/rails/view_helper.rb
+++ lib/transloadit/rails/view_helper.rb
@@ -6,10 +6,14 @@ module Transloadit::Rails::ViewHelper
   # to process.
   #
   def transloadit(template, options = {})
-    params    = Transloadit::Rails::Engine.template(template, options).to_json
-    signature = Transloadit::Rails::Engine.sign(params)
+    params = Transloadit::Rails::Engine.template(template, options).to_json
+    fields = hidden_field_tag(:params, params, :id => nil)
 
-    hidden_field_tag(:params,    params,    :id => nil) +
-    hidden_field_tag(:signature, signature, :id => nil)
+    if Transloadit::Rails::Engine.configuration['auth']['secret'].present?
+      signature = Transloadit::Rails::Engine.sign(params)
+      fields << hidden_field_tag(:signature, signature, :id => nil)
+    end
+
+    fields
   end
 end
